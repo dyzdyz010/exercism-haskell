1aaaf70fb37eb0503d478dc9dc7434010572acd3
Du Yizhuo <dyzdyz010@sina.com>
2018-05-22 17:55:03 +0800

    isogram another iteration

--- isogram/src/Isogram.hs
+++ isogram/src/Isogram.hs
@@ -1,11 +1,8 @@
 module Isogram (isIsogram) where
 
-import Data.List(sort)
+import Data.List(elemIndices)
 import Data.Char(isLetter, toLower)
 
 isIsogram :: String -> Bool
-isIsogram s = isIsogram' (sort [toLower x | x <- s, isLetter x]) ' '
-
-isIsogram' :: String -> Char -> Bool
-isIsogram' [] _ = True
-isIsogram' (x: xs) last = if x == last then False else isIsogram' xs x
+isIsogram s =
+  all (\x -> (length $ elemIndices x [toLower x | x <- s, isLetter x]) <= 1) ['a'..'z']

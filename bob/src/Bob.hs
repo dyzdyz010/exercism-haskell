0563b114addf837e9a3e8c59f5ffb8117ac8eb0e
Du Yizhuo <dyzdyz010@sina.com>
2018-05-23 18:06:32 +0800

    bob 2nd iteration

--- bob/src/Bob.hs
+++ bob/src/Bob.hs
@@ -1,5 +1,7 @@
 module Bob (responseFor) where
 
+import Data.Char (isLower, isUpper, isNumber)
+
 data Mood = Sure
           | Chillout
           | Calmdown
@@ -30,25 +32,15 @@ responseFor' [x] mood =
 responseFor' (x: xs) mood =
   if x == '?' then responseFor' xs Sure else
     case mood of
-      Sure -> if isLowercase x then responseFor' xs Whatever else responseFor' xs mood
-      Fine -> if isUppercase x then
+      Sure -> if isLower x then responseFor' xs Whatever else responseFor' xs mood
+      Fine -> if isUpper x then
                 responseFor' xs Chillout
-              else if isLowercase x then
+              else if isLower x then
                 responseFor' xs Whatever
               else
                 responseFor' xs mood
-      Chillout -> if isLowercase x then
+      Chillout -> if isLower x then
                     responseFor' xs Whatever
                   else
                     responseFor' xs mood
       _ -> responseFor' xs mood
-
-
-isLowercase :: Char -> Bool
-isLowercase x = elem x ['a'..'z']
-
-isUppercase :: Char -> Bool
-isUppercase x = elem x ['A'..'Z']
-
-isNumber :: Char -> Bool
-isNumber x = elem x ['0'..'9']

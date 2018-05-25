5c75183d47a044af3d2b8c2971524fc725ca3a38
Du Yizhuo <dyzdyz010@sina.com>
2018-05-25 08:36:07 +0800

    phone number

---
+++ phone-number/src/Phone.hs
@@ -0,0 +1,12 @@
+module Phone (number) where
+
+import Data.Char(isNumber)
+
+number :: String -> Maybe String
+number xs = case length nums of
+  11 -> let (a: b: _: _: c: _: _: ds) = nums in if validac a && validec b c then Just $ drop 1 nums else Nothing
+  10 -> let (b: _: _: c: _: _: ds) = nums in if validec b c then Just nums else Nothing
+  otherwise -> Nothing
+  where nums = filter isNumber xs
+        validac a = a == '1'
+        validec a b = all (`elem` ['2'..'9']) [a, b]

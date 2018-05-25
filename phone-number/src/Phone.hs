ef2c81b4471b81da82b3f169b6a3b4c5c24b4467
Du Yizhuo <dyzdyz010@sina.com>
2018-05-25 08:55:18 +0800

    phone number 2nd iteration

--- phone-number/src/Phone.hs
+++ phone-number/src/Phone.hs
@@ -4,8 +4,8 @@ import Data.Char(isNumber)
 
 number :: String -> Maybe String
 number xs = case length nums of
-  11 -> let (a: b: _: _: c: _: _: ds) = nums in if validac a && validec b c then Just $ drop 1 nums else Nothing
-  10 -> let (b: _: _: c: _: _: ds) = nums in if validec b c then Just nums else Nothing
+  11 -> let (a: b: _: _: c: _) = nums in if validac a && validec b c then Just $ drop 1 nums else Nothing
+  10 -> let (b: _: _: c: _) = nums in if validec b c then Just nums else Nothing
   otherwise -> Nothing
   where nums = filter isNumber xs
         validac a = a == '1'

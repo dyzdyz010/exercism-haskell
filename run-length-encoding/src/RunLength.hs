525a2be4ce7c875c43a9c8225ff160469d78705a
Du Yizhuo <dyzdyz010@sina.com>
2018-05-31 13:21:38 +0800

    run length encoding 2nd iteration

--- run-length-encoding/src/RunLength.hs
+++ run-length-encoding/src/RunLength.hs
@@ -1,6 +1,7 @@
 module RunLength (decode, encode) where
 
 import Data.Char(digitToInt, isNumber)
+import Data.List(concat, group)
 
 decode :: String -> String
 decode xs = decode' xs ' ' 1
@@ -12,11 +13,4 @@ decode' (x: xs) l c = case isNumber x of
   False -> (replicate c x) ++ decode' xs x 1
 
 encode :: String -> String
-encode text = encode' text ' ' 0
-
-encode' :: String -> Char -> Int -> String
-encode' [] _ 0 = []
-encode' [] l c = (if c /= 1 then show c else "") ++ [l]
-encode' (x: xs) l c = case x == l of
-  True -> encode' xs x $ c + 1
-  False -> (if c /= 0 then (if c /= 1 then show c else "") ++ [l] else "") ++ encode' xs x 1
+encode text = concat $ map (\x -> (if length x == 1 then "" else show $ length x)  ++ [x !! 0]) $ group text

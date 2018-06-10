bb5fcab048448485d0d4a1618f557dae9151a7a4
Du Yizhuo <dyzdyz010@sina.com>
2018-06-10 21:18:22 +0800

    prime factors 2nd iteration

--- prime-factors/src/PrimeFactors.hs
+++ prime-factors/src/PrimeFactors.hs
@@ -2,11 +2,13 @@ module PrimeFactors (primeFactors) where
 
 primeFactors :: Integer -> [Integer]
 primeFactors 1 = []
-primeFactors n = reverse $ primeFactors' n 2 []
+primeFactors n = primeFactors' n 2 []
 
 primeFactors' :: Integer -> Integer -> [Integer] -> [Integer]
 primeFactors' 1 _ list = list
-primeFactors' n c list = (if n `mod` c == 0 then
-                           (primeFactors' (n `div` c) 2 list) ++ [c] else
-                           primeFactors' n (c + 1) list
-                         ) ++ list
+primeFactors' n c list = list ++
+                         (if n `mod` c == 0 then
+                            c:  (primeFactors' (n `div` c) 2 list)
+                          else
+                            primeFactors' n (c + 1) list
+                         )
